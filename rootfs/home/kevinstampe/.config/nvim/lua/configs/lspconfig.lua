9dd083a19fb9164858fdfa5b3ffff5b2a5e556bb
Kevin Guldager Stampe <kevinstampe@gmail.com>
2026-08-03 10:11:49 +0200

    ee

--- rootfs/home/kevinstampe/.config/nvim/lua/configs/lspconfig.lua
+++ rootfs/home/kevinstampe/.config/nvim/lua/configs/lspconfig.lua
@@ -3,5 +3,17 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
-vim.lsp.config("roslyn", {})
+vim.lsp.enable("roslyn_ls")
+
+vim.lsp.config("roslyn_ls", {
+  filetypes = { "razor", "cs" },
+
+  settings = {
+    -- better performance
+    ["csharp|background_analysis"] = {
+      dotnet_analyzer_diagnostics_scope = "openFiles",
+      dotnet_compiler_diagnostics_scope = "openFiles",
+    },
+  },
+})
+
